ba8e14e72c672d6f1e027740d118e67f40bd27d8
Zack Winchell <zackwinchell@gmail.com>
2016-12-06 16:21:58 -0800

    Add ability to connect to a remote database

--- server/services/db.js
+++ server/services/db.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-
-mongoose.connect('mongodb://localhost/mvp');
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/mvp';
+mongoose.connect(MONGO_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error:'));
 db.once('open', function() {
-  console.log('Connected to MongoDB');
+  console.log(`Connected to MongoDB at ${process.env.MONGO_URI ? 'mLab' : 'localhost'}`);
 });
 
 module.exports = db;
